502bec2acc02dcdfceee5c87a4408d45632cfb60
saeed <saeedjefe@gmail.com>
2019-04-17 06:54:55 +0430

    Initial commit

---
+++ app/src/main/java/saeed/example/com/pushup/MainActivity.java
@@ -0,0 +1,21 @@
+package saeed.example.com.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+
+    private void pushUpTest()
+    {
+
+       int i = 2;
+
+    }
+
+}
